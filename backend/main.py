05e954e82b31a546286f6cde7fd7b4fff79bed05
MariusMagiera <mjmagiera174@gmail.com>
2021-11-26 21:58:49 +0100

    creation of postgres database with docker compose

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,46 @@
 from fastapi import FastAPI 
+from pydantic import BaseModel 
+from tortoise import fields 
+from tortoise.contrib.fastapi import register_tortoise 
+from tortoise.contrib.pydantic import pydantic_model_creator
+from tortoise.models import Model
+
 
 app = FastAPI() 
+
+class Task(BaseModel):
+    id: str
+    content: str
+
+
+class Tasks(BaseModel):
+    __root__: dict[str, Task]
+
+
+class Column(BaseModel):
+    id: str
+    title: str
+    taskIds: list
+
+
+class Columns(BaseModel):
+    __root__: dict[str, Task]
+
+
+class Board(BaseModel):
+    tasks: Tasks
+    columns: Columns
+    columnOrder: list
+
+
+class User(Model):
+    id = fields.IntField(pk=True)
+    username = fields.CharField(50, unique=True)
+    password = fields.CharField(200)
+    board = fields.JSONField(default={"tasks":{}, "columns": {}, "columnOrder":[]})
+
+User_Pydantic = pydantic_model_creator(User, name='User')
+UserIn_Pydantic = pydantic_model_creator(User, name="UserIn", exclude_readonly=True, exclude=('board'))
 
 @app.get('/board')
 def get_board(): 
@@ -25,3 +65,11 @@ def get_board():
         'columnOrder': ['column-1','column-2']
     }
     return {'board': board_data}
+
+register_tortoise(
+    app, 
+    db_url="postgres://postgres:Password1@127.0.0.1:5432/postgres",
+    modules={"models": ["main"]},
+    generate_schemas=True,
+    add_exception_handlers=True,
+)
